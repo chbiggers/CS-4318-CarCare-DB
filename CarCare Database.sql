52c7d00e8589099b806b5652e5a863867aba3703
Nick <43190085+chbiggers@users.noreply.github.com>
2019-04-15 17:16:05 -0500

    Tables: Repairs, Administrators, Technicians, and Customers

---
+++ CarCare Database.sql
@@ -0,0 +1,7 @@
+INSERT INTO Administrators (AdministratorFName, AdministratorLName)
+Values
+	('Elizabeth', 'Morrow'),
+	('Wesley', 'Newport'),
+	('Shane', 'Sharper'),
+	('Shannon', 'Dennis'),
+	('Carter', 'Morrison'); 
